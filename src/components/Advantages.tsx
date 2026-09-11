520fd3ca5f087672e7ca055d503ab838052a95cf
Fatima Tufail <fatimatufail0218@gmail.com>
2026-09-11 10:24:50 +0500

    add project to the github

--- src/components/Advantages.tsx
+++ src/components/Advantages.tsx
@@ -2,7 +2,24 @@ import React from 'react'
 
 const Advantages = () => {
   return (
-    <section>Advantages</section>
+    <section className='w-full bg-[#F5F9FC] lg:min-h-[90vh]'>
+      <div className=' max-w-content flex flex-col md:flex-row'>
+        {/* ---------------------------------------text box------------------------------------------------- */}
+          <div>
+            <h2 className=''>
+              Waarom Lokale Ondernemers Ons Kiezen
+            </h2>
+          </div>
+        {/* -------------------------------------------------------hand image------------------------------------------- */}
+          <div>
+            
+          </div>
+        {/* -----------------------------------------------3 box grid---------------------------------------------- */}
+        <div>
+
+        </div>
+      </div>
+    </section>
   )
 }
 
